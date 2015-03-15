9380aa0f49bec85c20ada12d48d6a8ef53285111
DapperPixpy <yugioh123>
2015-03-15 00:13:13 -0700

    1

--- data.js
+++ data.js
@@ -1,48 +1,49 @@
-//GLOBAL Thingies
 var date = new Date();
 var year = date.getFullYear();
 
-var c_firstName, c_lastName;
-var c_dobMonth, c_dobDay, c_dobYear;
+var c_firstName;
+var c_lastName;
+var c_dobMonth;
+var c_dobDay;
+var c_dobYear;
 var c_retirementAge;
 var c_comments;
 var c_age;
 
-var s_firstName, s_lastName;
-var s_dobMonth, s_dobDay, s_dobYear;
+var s_firstName
+var s_lastName;
+var s_dobMonth;
+var s_dobDay;
+var s_dobYear;
 var s_retirementAge;
 var s_comments;
 var s_age;
 
-var a_firstName, a_lastName;
-var a_dobMonth, a_dobDay, a_dobYear;
-var a_retirementAge;
-var a_comments;
-		
-
-
-function saveClientData() {
+function saveClientData(element) {
 	queryClientInfo();
-    var file = "Client Data";
-    window.open('data:txt/file;charset=utf-8,' + JSON.stringify(getClientJSON()));
+	var json = JSON.stringify(getClientJSON(), null, "\t");
+	var blob = new Blob([json], {type: "application/json"});
+	var url = URL.createObjectURL(blob);
+    element.download = "Client Data.json";
+	element.href = url;
 }
 
 function queryClientInfo() {
     //client
-    c_firstName = document.getElementByID("cFName").value;
-    c_lastName = document.getElementByID("cLName").value;
-    c_dobMonth = document.getElementByID("cMonth").value;
-    c_dobDay = document.getElementByID("cDay").value;
-    c_dobYear = document.getElementByID("cYear").value;
-    c_comments = document.getElementByID("cComment").value;
+    c_firstName = document.getElementById("cFName").value;
+    c_lastName = document.getElementById("cLName").value;
+    c_dobMonth = document.getElementById("cMonth").value;
+    c_dobDay = document.getElementById("cDay").value;
+    c_dobYear = document.getElementById("cYear").value;
+    c_comments = document.getElementById("cComment").value;
 
     //spouse
-    s_firstName = document.getElementByID("sFName").value;
-    s_lastName = document.getElementByID("sLName").value;
-    s_dobMonth = document.getElementByID("sMonth").value;
-    s_dobDay = document.getElementByID("sDay").value;
-    s_dobYear = document.getElementByID("sYear").value;
-    s_comments = document.getElementByID("sComment").value;
+    s_firstName = document.getElementById("sFName").value;
+    s_lastName = document.getElementById("sLName").value;
+    s_dobMonth = document.getElementById("sMonth").value;
+    s_dobDay = document.getElementById("sDay").value;
+    s_dobYear = document.getElementById("sYear").value;
+    s_comments = document.getElementById("sComment").value;
 }
 		
 function getClientJSON() {
@@ -63,13 +64,5 @@ function getClientJSON() {
     json["s_dobYear"] = s_dobYear;
     json["s_retirementAge"] = s_retirementAge;
     json["s_comments"] = s_comments;
-    //ASSET'S INFO
-    json["a_firstName"] = a_firstName;
-    json["a_lastName"] = a_lastName;
-    json["a_dobMonth"] = a_dobMonth;
-    json["a_dobDay"] = a_dobDay;
-    json["a_dobYear"] = a_dobYear;
-    json["a_retirementAge"] = a_retirementAge;
-    json["a_comments"] = a_comments;
     return json;
 }
