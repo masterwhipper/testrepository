ad0f612c1c18386a96853314075edead18a71433
masterwhipper <master_whipper@aol.com>
2015-03-14 21:24:06 -0500

    1

--- report.js
+++ report.js
@@ -1,17 +1,20 @@
+var cAge = 60;
+var sAge = 67;
 
 
 function generateReport() {
-var i = 0;
+for (i = 0; i < 21; i++) { 
 // Create an empty <tr> element and add it to the 1st position of the table:
-var row = table.insertRow(0);
+var rTable = document.getElementById("incomeTable");
+var row = rTable.insertRow(i);
 
 // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
 var yrCell = row.insertCell(0);
 var cAgeCell = row.insertCell(1);
-var sAgeCell = row.insertCell(1);
-var pretaxCell = row.insertCell(1);
-var taxableCell = row.insertCell(1);
-var aftertaxCell = row.insertCell(1);
+var sAgeCell = row.insertCell(2);
+var pretaxCell = row.insertCell(3);
+var taxableCell = row.insertCell(4);
+var aftertaxCell = row.insertCell(5);
 
 // Add some text to the new cells:
 yrCell.innerHTML = year + i;
@@ -20,4 +23,5 @@ sAgeCell.innerHTML = sAge + i;
 pretaxCell.innerHTML = "test";
 taxableCell.innerHTML = "test";
 aftertaxCell.innerHTML = "test";
+}
 }
