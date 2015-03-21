ff2fbaa0112a2c41f45c82db66d6256a2dc78316
masterwhipper <master_whipper@aol.com>
2015-03-20 23:10:56 -0500

    1

--- sources.js
+++ sources.js
@@ -1,16 +1,39 @@
-function addIncum(owner, description, monthly, annual) {
- var table = document.getElementById("incumTable");
- var row = table.insertRow(i);
+function addIncum(object) {
+ var table = document.getElementById("assetTable");
+ var row = table.insertRow(incomeSources.length - 1);
  var c1 = row.insertCell(0);
  var c2 = row.insertCell(1); 
  var c3 = row.insertCell(2);
  var c4 = row.insertCell(3);
 
+ var owner = object["aOwner"];
+ var description = object["aDesc"];
+ var monthly = object["aMA"];
+ 
 c1.innerHTML = owner,
 c2.innerHTML = description,
 c3.innerHTML = monthly,
 c4.innerHTML = monthly * 12;
 }
+
+/*function addIncum() {
+ alert("bitchtits");
+ var table = document.getElementById("assetTable");
+ var row = table.insertRow(0);
+ var c1 = row.insertCell(0);
+ var c2 = row.insertCell(1); 
+ var c3 = row.insertCell(2);
+ var c4 = row.insertCell(3);
+
+ var owner = "tits";
+ var description = "boobs";
+ var monthly = 12;
+ 
+c1.innerHTML = owner,
+c2.innerHTML = description,
+c3.innerHTML = monthly,
+c4.innerHTML = monthly * 12;
+}*/
 
 function addAsset(owner, description, e) {
  var table = document.getElementById("incumTable");
@@ -23,3 +46,6 @@ c1.innerHTML = owner,
 c2.innerHTML = description,
 c3.innerHTML = e;
 }
+
+getCurrentAsset()
+getCurrentIncomeSource()
