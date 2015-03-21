c2534bab345b5086a4de736c37f7ec63f61cf0d0
masterwhipper <master_whipper@aol.com>
2015-03-20 22:31:44 -0500

    3
    3

--- math.js
+++ math.js
@@ -1,4 +1,4 @@
-in all formulas I give you y is output to column
+/*in all formulas I give you y is output to column
 
 monthly amount = b, COLA = r, survivor benefit = a, x = years
 
@@ -9,7 +9,7 @@ r and b always static
 FORMULA
 ==========
 y=[12b((1+r)^x)]a
-[1/3/2015 3:40:46 PM] Masto: ^that's all for an income source thing
+[1/3/2015 3:40:46 PM] Masto: ^that's all for an income source thing*/
 
 function incomesource(b,r,x,a){
     var c = r+1;
