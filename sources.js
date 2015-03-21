36691e616afba83d1f9a1553b8b5f1c964136936
DapperPixpy <yugioh123>
2015-03-21 00:11:21 -0700

    f

--- sources.js
+++ sources.js
@@ -1,3 +1,4 @@
+/*
 function addIncum(owner, description, monthly, annual) {
  var table = document.getElementById("incumTable");
  var row = table.insertRow(i);
@@ -7,6 +8,23 @@ function addIncum(owner, description, monthly, annual) {
  var c4 = row.insertCell(3);
 
 c1.innerHTML = owner,
+c2.innerHTML = description,
+c3.innerHTML = monthly,
+c4.innerHTML = monthly * 12;
+}
+*/
+
+function addIncum() {
+ var table = document.getElementById("incumTable");
+ var row = table.insertRow(i);
+ var c1 = row.insertCell(0);
+ var c2 = row.insertCell(1); 
+ var c3 = row.insertCell(2);
+ var c4 = row.insertCell(3);
+
+ var incomeJson = getCurrentIncomeSource();
+ 
+c1.innerHTML = incomeJson["cOwner]",
 c2.innerHTML = description,
 c3.innerHTML = monthly,
 c4.innerHTML = monthly * 12;
